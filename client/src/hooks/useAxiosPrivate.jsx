80d98972fdfc299d446958ea2e2ca0075b803c74
ferhat <ferdiag@yahoo.de>
2022-10-24 14:05:41 +0200

    commit

--- client/src/hooks/useAxiosPrivate.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { axiosPrivate } from '../assets/api/axios';
-
-const useAxiosPrivate = () => {
-  return <div>useAxiosPrivate</div>;
-};
-
-export default useAxiosPrivate;
